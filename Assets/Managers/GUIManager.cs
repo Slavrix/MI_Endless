4fee65efddfb6418be42113f390e10c842421002
Nicholas Tsaoucis <rusty.nt@gmail.com>
2017-02-22 22:36:03 +1100

    Initial

---
+++ Assets/Managers/GUIManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+	
+	public GUIText gameOverText, instructionsText, runnerText;
+
+	void Start () {
+		GameEventManager.GameStart += GameStart;
+		GameEventManager.GameOver += GameOver;
+		gameOverText.enabled = false;
+	}
+	
+	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			GameEventManager.TriggerGameStart();
+		}
+	}
+
+	private void GameStart () {
+		gameOverText.enabled = false;
+		instructionsText.enabled = false;
+		runnerText.enabled = false;
+		enabled = false;
+	}
+
+	private void GameOver () {
+		gameOverText.enabled = true;
+		instructionsText.enabled = true;
+		enabled = true;
+	}
+}
