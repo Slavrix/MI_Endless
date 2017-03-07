4f860907e426688267db5e50b21b963a0c636aba
SuleymanO <tatsu7@live.com>
2017-03-07 17:31:28 +1100

    Basic Daily Reward Spinner
    added chest that spins out of 10 items with specified chances per item

---
+++ Assets/Sol's Fol's/Scripts/SelectRandomChest.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChestState {closed = 0, open };
+
+public class SelectRandomChest : MonoBehaviour {
+
+    public GameObject [ ] rewardItemArray;
+
+    public ChestState chestState;
+
+    public Sprite chestClosed;
+    public Sprite chestOpen;
+    SpriteRenderer sr;
+
+    [Tooltip ( "offset value for the creation location of generated item" )]
+    public Vector3 offsetPos;
+
+
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer> ( );
+    }
+
+    //void OnEnable ()
+    //{
+    //    chestState = ChestState.closed;
+    //    sr.sprite = chestClosed;
+    //}
+
+
+    void OnMouseDown ( )
+
+    {
+        //change the sprite
+        //instantiate item
+
+        if(this.chestState == ChestState.closed)
+        {
+            chestState = ChestState.open;
+            sr.sprite = chestOpen;
+            Debug.Log ( chestState );
+            spawnRandomItem ( );
+
+        }
+        else
+        {
+            chestState = ChestState.closed;
+            sr.sprite = chestClosed;
+            Debug.Log ( chestState );
+
+        }
+
+    }
+
+    void spawnRandomItem()
+    {
+        float totalItemWeight = 0;
+
+        for ( int i = 0; i < rewardItemArray.Length; i++ )
+        {
+
+            itemScript itemCompScript = rewardItemArray [ i ].GetComponent<itemScript> ( );
+            totalItemWeight += itemCompScript.rarityWeight;
+
+        }
+
+        float rolledNumber = Random.Range ( 1, totalItemWeight );
+        totalItemWeight = 0; //clear totalweight
+
+
+        for ( int i = 0; i < rewardItemArray.Length; i++ )
+        {
+            Debug.Log ( "-------------------------------loop " + i + " of " + rewardItemArray.Length + "---------------------------------------" );
+
+            itemScript itemCompScript = rewardItemArray [ i ].GetComponent<itemScript> ( );
+            Debug.Log ( "loop at " + itemCompScript.gameObject.name + " at index" + i );
+            Debug.Log ( "total weight is " + totalItemWeight + " item weight of " + itemCompScript.rarityWeight );
+
+            Debug.Log ( "item " + itemCompScript.gameObject.name + " range is " + totalItemWeight + " to " + (totalItemWeight + itemCompScript.rarityWeight) );
+
+            if(rolledNumber <= totalItemWeight + itemCompScript.rarityWeight )
+            {
+                Vector3 pos = transform.position + offsetPos;
+                Instantiate ( rewardItemArray [ i ], pos, transform.rotation );
+                Debug.Log ( "spawning " + rewardItemArray [ i ].name );
+
+                break;
+                
+            }
+
+            totalItemWeight += itemCompScript.rarityWeight;
+
+            Debug.Log ( "-------------------------------loop " + i + " of " + rewardItemArray.Length + " ended --------------------------------" );
+
+        }
+    }
+
+}
---
+++ Assets/Sol's Fol's/Scripts/itemScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemScript : MonoBehaviour {
+
+    public string itemName;
+
+    public int itemID;
+    
+    [Range(0f,100f)]
+    public float rarityWeight; // weight to add for item drop probability
+
+    [Tooltip ( "delay for item to be destroyed" )]
+    public float delay = 1.15f;
+
+    void OnEnable()
+    {
+        Destroy ( gameObject, delay );
+    }
+    
+}
